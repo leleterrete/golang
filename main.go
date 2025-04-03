8ffa6ae85d6e3ef2b87e6614e9629f180df9bac4
leleterrete <leticia.t.souza7@aluno.senai.br>
2025-04-03 09:19:10 -0300

    slice

--- main.go
+++ main.go
@@ -9,5 +9,22 @@ import "fmt"
 	fmt.Println(nomes)
     nomes[3] = " Clark Kent"
 	fmt.Println(nomes)
+	// SLICE
+	var score = []int{100,200,300,400}
+    fmt.Println(score)
+	score[1] = 2
+	fmt.Println(score)
+	fmt.Println(score, len(score), cap(score))
+	rangeOne := score[1:3]
+	fmt.Println(rangeOne)	
+	rangeTwo := score[2:]
+	fmt.Println(rangeTwo)
+	rangeThree := score [:3]
+	fmt.Println(rangeThree)
+	var superherois = []string{"DeadPool", "Homem-Aranha","Motoqueiro Fantasma"}
+	fmt.Println(superherois)
+	superherois =append(superherois, "Bem 10","Homem de Ferro", "Capitão america","Viuva Negra")
+	fmt.Println(superherois, len(superherois), cap(superherois))
+	
 }
 
