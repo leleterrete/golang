416a48e10a18e4cecfc6510ae1c87b829be5d22e
leleterrete <leticia.t.souza7@aluno.senai.br>
2025-04-04 08:23:19 -0300

    bibliotecas padrões

--- main.go
+++ main.go
@@ -1,30 +1,27 @@
 package main
 
-import "fmt"
+import (
+"fmt"
+"strings"
+"sort"
+)
 
  func main(){
-	var ages = [4]int{17,16,20,40}
-	nomes := [4]string{"Mario", "Luigi", "DeadPool", "Superman"}
-	fmt.Println(ages)
-	fmt.Println(nomes)
-    nomes[3] = " Clark Kent"
-	fmt.Println(nomes)
-	// SLICE
-	var score = []int{100,200,300,400}
-    fmt.Println(score)
-	score[1] = 2
-	fmt.Println(score)
-	fmt.Println(score, len(score), cap(score))
-	rangeOne := score[1:3]
-	fmt.Println(rangeOne)	
-	rangeTwo := score[2:]
-	fmt.Println(rangeTwo)
-	rangeThree := score [:3]
-	fmt.Println(rangeThree)
-	var superherois = []string{"DeadPool", "Homem-Aranha","Motoqueiro Fantasma"}
-	fmt.Println(superherois)
-	superherois =append(superherois, "Bem 10","Homem de Ferro", "Capitão america","Viuva Negra")
-	fmt.Println(superherois, len(superherois), cap(superherois))
-	
+
+   greeting := "Hello my friends!"
+   fmt.Println(strings.Contains(greeting,"dog"))
+fmt.Println(strings.ReplaceAll(greeting,"my ","mine "))
+fmt.Println(strings.ToUpper(greeting))
+fmt.Println(strings.Index(greeting,"my"))
+fmt.Println(strings.Split(greeting," "))
+ages := []int{50,80,10}
+sort.Ints(ages)
+fmt.Println(ages)
+index := sort.SearchInts(ages,50)
+fmt.Println(index)
+names :=[]string{"Caroline", "Maicon", "Diego"}
+sort.Strings(names)
+fmt.Println(names)
+fmt.Println(sort.SearchStrings(names,"Caroline"))
 }
 
