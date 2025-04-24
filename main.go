c7894f5654f26b1538c48c20d5c6d6130f448b8c
leleterrete <leticia.t.souza7@aluno.senai.br>
2025-04-24 14:04:36 -0300

    notas + media

--- main.go
+++ main.go
@@ -3,15 +3,32 @@ package main
 import (
 	"fmt"
 )
-func pegarNome()(string,string){
-return "Barry", "Allen"
+func analisarNotas(nota1, nota2 float64) (float64, string) {
+  
+media := ( nota1 + nota2)/2
+var resultado string
+
+    if media >= 6 {
+        resultado = "Aprovado"
+    } else {
+        resultado = "Reprovado"
+    }
+
+    return media, resultado
 
 }
-  func main(){
-    nome, sobrenome := pegarNome()
-    fmt.Println(nome)
-    fmt.Println(sobrenome)
-  }
 
+func main() {
+  var nota1 float64
+  var nota2 float64
+  fmt.Println("Digite sua primeira nota:")
+  fmt.Scan(&nota1)
+  fmt.Println("Digite sua segunda nota:")
+  fmt.Scan(&nota2)
+  media, resultado := analisarNotas(nota1 , nota2)
+  fmt.Println("Média:", media)
+  fmt.Println("Resultado:", resultado)
+}
+  
 
   
