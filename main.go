eec883be2f6c36c3e2153d3c04f740b8609a7e07
leleterrete <leticia.t.souza7@aluno.senai.br>
2025-04-24 13:27:58 -0300

    nome e sobrenome

--- main.go
+++ main.go
@@ -3,24 +3,14 @@ package main
 import (
 	"fmt"
 )
+func pegarNome()(string,string){
+return "Barry", "Allen"
 
+}
   func main(){
-    alunoIdade := make(map[string]int)
-    alunoIdade["Bruno"] = 15
-    alunoIdade["Otavio"]= 16
-    alunoIdade["Fabiano"] = 40
-    alunoIdade["Isabela"] = 15
-    fmt.Println("Idade do Bruno", alunoIdade["Bruno"])
-    notasAlunos :=map[string]float64{
-        "Bruno" : 9.7,
-        "Otavio" :10,
-        "Fabiano":8.7,
-        "Isabela": 9.5,
-
-    }
-    for k, v:= range notasAlunos {
-        fmt.Printf("%s tirou a nota % 1.f ", k ,v )
-    }
+    nome, sobrenome := pegarNome()
+    fmt.Println(nome)
+    fmt.Println(sobrenome)
   }
 
 
